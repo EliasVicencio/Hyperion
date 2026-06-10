2cfc1a862d8ba595c0fd7dc34708707ee3d243b6
EliasVicencio <elias.vicencio2505@gmail.com>
2026-06-10 18:57:23 -0400

    Se importa la inicialización de main

--- backend/main.py
+++ backend/main.py
@@ -264,3 +264,10 @@ async def external_dashboard(token: str = None):
         </body>
     </html>
     """
+    
+if __name__ == "__main__":
+
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000) 
+
